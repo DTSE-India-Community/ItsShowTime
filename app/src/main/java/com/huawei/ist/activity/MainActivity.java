71fc4b8fbf6c29d973ee21ba13158c27aea6eb94
Sanghati Mukherjee <sWX914037@china.huawei.com>
2020-07-20 17:26:49 +0530

    first commit

---
+++ app/src/main/java/com/huawei/ist/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.huawei.ist.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huawei.ist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
