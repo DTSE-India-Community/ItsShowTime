b959dc6bc123502b2579e94faa7089fc14a0185e
sujith <tushar.srivastava@huawei.com>
2020-07-22 08:03:47 +0530

    Added Xml

---
+++ app/src/main/java/com/huawei/ist/activity/SplashActivty.java
@@ -0,0 +1,16 @@
+package com.huawei.ist.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huawei.ist.R;
+
+public class SplashActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
