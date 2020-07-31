27dcd6e7ce4f94cb7130c51158d1fd308ccae2c6
Sanghati <sanghati.mukherjee@huawei.com>
2020-07-31 09:31:41 +0530

    just commit

--- app/src/main/java/com/huawei/ist/activity/PaymentActivity.java
+++ app/src/main/java/com/huawei/ist/activity/PaymentActivity.java
@@ -8,40 +8,66 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 import com.huawei.ist.R;
 import com.huawei.ist.network.GetDataService;
 import com.huawei.ist.network.RetrofitClientInstance;
+import com.huawei.ist.utility.Constant;
 import com.huawei.ist.utility.util.JweUtil;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PaymentActivity extends FragmentActivity {
 
     private String seats;
     private String passObject = "";
-    private String issuerId = "";
+    private String issuerId = "102594783";
     private String browserUrl = "https://walletpass-dra.cloud.huawei.com/walletkit/consumer";
-    private TextView mMovieName;
+    private TextView mMovieName,txtHeader;
     private TextView mMovieBanner;
     private TextView mSelectedSeats;
     private TextView mTotalAmount;
-
+    private ProgressBar progressBar;
+    private GetDataService service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_payment);
         getDetails();
+        txtHeader = findViewById(R.id.txtHeader);
+        progressBar = findViewById(R.id.progressBar);
+        txtHeader.setText("PAYMENT DETAILS");
+        txtHeader.setTypeface(Constant.getTypeface(this,1));
         mMovieName = findViewById(R.id.txtMovieName);
         mMovieBanner = findViewById(R.id.txtMovieBanner);
         mSelectedSeats = findViewById(R.id.txtSeatNo);
         mTotalAmount = findViewById(R.id.txtPrice);
-        mMovieName.setText("Movie : " + " sdsdsd");
-        mMovieBanner.setText("Theater Name : " + " sdsdsd");
+        mMovieName.setText("Movie : " + " Dil Bechara");
+        mMovieBanner.setText("Theater Name : " + " PVR CINEMAS");
         mSelectedSeats.setText("Selected Seats : " + seats);
-        mTotalAmount.setText("100 " + "RS");
-        generateJWEStr();
-        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        mTotalAmount.setText("200 " + "RS");
+        //generateJWEStr();
+        service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
 
     }
 
@@ -55,6 +81,7 @@ public class PaymentActivity extends FragmentActivity {
     private void generateJWEStr() {
         String jweStr = getJweFromAppServer(passObject);
         Intent intent = new Intent(Intent.ACTION_VIEW);
+
         intent.setData(Uri.parse(browserUrl + "/pass/save?jwt=" + Uri.encode(jweStr)));
         try {
             startActivity(intent);
@@ -68,15 +95,52 @@ public class PaymentActivity extends FragmentActivity {
         try {
             jweStr = JweUtil.generateJwe(issuerId, passObject);
         } catch (Exception e) {
-            Toast.makeText(this, "fail ：jwe trans error", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            System.out.println("ERROR >>>>" + e.getMessage().toString());
             Log.i("TAG", "jwe trans error");
             return null;
         }
         Log.i("TAG", "jweStr:" + jweStr);
+        System.out.println("JWE >>>" + jweStr);
         return jweStr;
     }
 
     public void proceedToPay(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+        HashMap<String, String> ticket = new HashMap<>();
+        ticket.put("seat", seats);
+        ticket.put("username","Sanghati Mukherjee");
+        Call<Object> call = service.createMovieTicket(ticket);
+        call.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
 
+
+
+//                passObject = new Gson().toJson(JSON.toJSONString(response.body()));
+//
+//                System.out.println("RESPONSE >>> " + passObject);
+//                if(passObject!=null) {
+//                    generateJWEStr();
+//                }
+                passObject = new Gson().toJson(JSON.toJSONString(response.body()));
+
+                    System.out.println("RESPONSE >>> " + passObject);
+
+
+                progressBar.setVisibility(View.GONE);
+                generateJWEStr();
+
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+                Toast.makeText(PaymentActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                System.out.println("RESPONSE >>> " + t.getMessage());
+
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+       // generateJWEStr();
     }
 }
--- app/src/main/java/com/huawei/ist/network/GetDataService.java
+++ app/src/main/java/com/huawei/ist/network/GetDataService.java
@@ -2,10 +2,14 @@ package com.huawei.ist.network;
 
 import com.huawei.ist.model.LoginModel;
 
+import java.util.HashMap;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface GetDataService {
-    @POST("/api")
-    Call<LoginModel> getAllPhotos();
+
+    @POST("/createTicket")
+    Call<Object> createMovieTicket(@Body HashMap<String, String> map);
 }
